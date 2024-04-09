224fc3b3aa759c5144fd7094f8da8bf9f6e4dd9d
Gattony <duc.nm10@st.buv.edu.vn>
2024-04-09 11:47:36 +0700

    update

--- MarioBaseProject/Source.cpp
+++ MarioBaseProject/Source.cpp
@@ -10,7 +10,35 @@ using namespace std;
 SDL_Window* g_window = nullptr;
 
 //Function prototypes
-bool InitSDL();
+bool InitSDL()
+{
+	// Setup SDL
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error:" << SDL_GetError();
+		return false;
+	}
+
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error:" << SDL_GetError();
+			return false;
+		}
+	}
+}
+
 void CloseSDL();
 
 int main(int argc, char* args[])
