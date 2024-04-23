719b811a5adca6c4432767b3ba38ddae926b1021
Gattony <duc.nm10@st.buv.edu.vn>
2024-04-23 11:47:33 +0700

    first up

---
+++ MarioBaseProject/GameScreen.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _GAMESCREEN_H
+#define _GAMESCREEN_H
+
+using namespace std;
+#include <SDL.h>
+
+class GameScreen
+{
+	SDL_Renderer* m_renderer;
+	
+	GameScreen(SDL_Renderer* renderer);
+	~GameScreen();
+
+	virtual void Render();
+	virtual void Update(float deltaTime, SDL_Event e);
+};
+
+#endif //0_GAMESCREENCLASS_H
