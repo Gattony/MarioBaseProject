635fd6285373ac7fb6314ecc98870dec0f8b241a
Gattony <duc.nm10@st.buv.edu.vn>
2024-05-07 22:37:27 +0700

    up up up

--- MarioBaseProject/Collisions.cpp
+++ MarioBaseProject/Collisions.cpp
@@ -8,7 +8,7 @@ Collisions* Collisions::m_instance = nullptr;
 
 Collisions::Collisions()
 {
-
+	m_collision_radius = 15.0f;
 }
 
 Collisions::~Collisions()
@@ -23,4 +23,18 @@ Collisions* Collisions::Instance()
 		m_instance = new Collisions;
 	}
 	return m_instance;
+}
+
+float Collisions::GetCollisionRadius()
+{
+	return m_collision_radius;
+}
+
+bool Collisions::Circle(Character* character1, Character* character2)
+{
+	double distance = sqrt((vec.x * vec.x) + (vec.y * vec.y));
+	double combined_distance = (character1->GetCollisionRadius() + character2->GetCollisionRadius());
+	Vector2D vec = Vector2D((character1->GetPosition().x - character2->GetPosition().x), character1->GetPosition().y - character2->GetPosition().y);
+
+	return distance < combined_distance;
 }
--- MarioBaseProject/Collisions.h
+++ MarioBaseProject/Collisions.h
@@ -12,12 +12,18 @@ class Collisions
           ~Collisions();
           static Collisions* Instance();
 
+          float GetCollisionRadius();
           bool Circle(Character* character1, Character* character2);
           bool Box(Rect2D rect1, Rect2D rect2);
+     
+     protected:
+         float m_collision_radius;
+
      private:
          Collisions();
 
          static Collisions* m_instance;
+
 };
 
 #endif //_COLLISONS_H
