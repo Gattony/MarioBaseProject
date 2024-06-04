95b76192b4fab19a33d30c7c9df85460816d987d
Gattony <duc.nm10@st.buv.edu.vn>
2024-06-04 11:50:49 +0700

    font and uses

--- MarioBaseProject/GamescreenIntro.cpp
+++ MarioBaseProject/GamescreenIntro.cpp
@@ -15,6 +15,8 @@ GamescreenIntro::~GamescreenIntro()
 
 bool GamescreenIntro::SetUpLevel()
 {
+	titleText = new TextRenderer(50);
+
 	//load texture
 	m_background_texture = new Texture2D(m_renderer);
 
@@ -31,6 +33,8 @@ void GamescreenIntro::Render()
 {
 	//draw the background
 	SDL_SetRenderDrawColor(m_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+	titleText->Render(m_renderer, "MARIO GAME!", SCREEN_WIDTH / 4 - 50, SCREEN_HEIGHT / 4);
 }
 
 void GamescreenIntro::Update(float deltaTime, SDL_Event e)
